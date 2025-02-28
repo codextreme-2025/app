fb2e4f8067e972ca31a2d9c747bdb4ee308f416d
Angelo Verlain <hey@vixalien.com>
2025-02-28 10:47:46 +0000

    feat: add some classes

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,13 +1,69 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
-import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+import {
+  Canvas,
+  Group,
+  listFontFamilies,
+  matchFont,
+  Rect,
+  Text as SkiaText,
+} from "@shopify/react-native-skia";
+
+const fontFamily = Platform.select({ ios: "Helvetica", default: "serif" });
+const fontStyle = {
+  fontFamily,
+  fontSize: 14,
+  fontStyle: "italic",
+  fontWeight: "bold",
+} as const;
+const font = matchFont(fontStyle);
+
+export interface Node {
+  type: string;
+}
+
+export interface Building extends Node {
+  type: "building";
+}
+
+export interface Room extends Node {
+  type: "room";
+  name: string;
+  subType?: "classroom";
+  position: [number, number];
+  size: [number, number];
+}
+
+export const classes: Room[] = [
+  {
+    type: "room",
+    name: "Guinea",
+    subType: "classroom",
+    position: [0, 100],
+    size: [100, 100],
+  },
+  {
+    type: "room",
+    name: "Mozambique",
+    subType: "classroom",
+    position: [200, 0],
+    size: [100, 100],
+  },
+  {
+    type: "room",
+    name: "Malawi",
+    subType: "classroom",
+    position: [300, 0],
+    size: [100, 100],
+  },
+];
 
 export default function TabTwoScreen() {
-  const width = 256;
-  const height = 256;
+  const width = 400;
+  const height = 400;
   const r = width * 0.33;
 
   return (
@@ -19,11 +75,40 @@ export default function TabTwoScreen() {
         darkColor="rgba(255,255,255,0.1)"
       />
       <Canvas style={{ width, height }}>
-        <Group blendMode="multiply">
-          <Circle cx={r} cy={r} r={r} color="cyan" />
-          <Circle cx={width - r} cy={r} r={r} color="magenta" />
-          <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
-        </Group>
+        {classes.map((classRoom) => {
+          const [x, y] = classRoom.position;
+          const [width, height] = classRoom.size;
+
+          const textSize = font.measureText(classRoom.name);
+
+          return (
+            <Group blendMode="multiply">
+              <Rect
+                x={x}
+                y={y}
+                width={width}
+                height={height}
+                color="pink"
+                style="fill"
+              />
+              <Rect
+                x={x}
+                y={y}
+                width={width}
+                height={height}
+                color="black"
+                style="stroke"
+                strokeWidth={2}
+              />
+              <SkiaText
+                font={font}
+                x={x + (width / 2 - textSize.width / 2)}
+                y={y + (height / 2 + textSize.height / 2)}
+                text={classRoom.name}
+              />
+            </Group>
+          );
+        })}
       </Canvas>
     </View>
   );
